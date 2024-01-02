77bf2bc4770d7b6f066b33c32cf06fa810bbbbef
VICooL <1764961762@qq.com>
2024-01-02 23:52:32 +0800

    fix: 自定义SQL

--- mp-demo/src/main/java/com/itheima/mp/mapper/UserMapper.java
+++ mp-demo/src/main/java/com/itheima/mp/mapper/UserMapper.java
@@ -1,12 +1,18 @@
 package com.itheima.mp.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.toolkit.Constants;
 import com.itheima.mp.domain.po.User;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
+@Mapper
 public interface UserMapper extends BaseMapper<User> {
 
     List<User> queryUserByIds(@Param("ids") List<Long> ids);
+
+    void customerSqlUpdate(@Param(Constants.WRAPPER) LambdaQueryWrapper<User> wrapper,@Param("amount") int amount);
 }
--- mp-demo/src/test/java/com/itheima/mp/mapper/UserMapperTest.java
+++ mp-demo/src/test/java/com/itheima/mp/mapper/UserMapperTest.java
@@ -1,6 +1,8 @@
 package com.itheima.mp.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.itheima.mp.domain.po.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +21,7 @@ class UserMapperTest {
     @Test
     void testInsert() {
         User user = new User();
-//        user.setId(5L);
+        // user.setId(5L);
         user.setUsername("Taka");
         user.setPassword("123");
         user.setPhone("18688990012");
@@ -70,7 +72,7 @@ class UserMapperTest {
      * @Date: 2023/12/25
      */
     @Test
-    void testQueryWrapper(){
+    void testQueryWrapper() {
         // 1.构造查询条件
         QueryWrapper<User> wrapper = new QueryWrapper<User>()
                 .select("id", "username", "info", "balance")
@@ -88,41 +90,72 @@ class UserMapperTest {
      * @Author: VICooL
      */
     @Test
-    void testUpdateByQueryWrapper(){
+    void testUpdateByQueryWrapper() {
         // 1.要更新的数据
         User user = new User();
-        user.setBalance (2000);
+        user.setBalance(2000);
         // 2.更新的条件
         QueryWrapper<User> wrapper = new QueryWrapper<User>()
-                .eq("username","jack");
+                .eq("username", "jack");
         // 3.执行更新
-        userMapper.update(user,wrapper);
+        userMapper.update(user, wrapper);
     }
 
     /**
-     * @Description: 更新id为1,2,4的用户的余额,扣200
+     * @Description: 更新id为1, 2, 4的用户的余额, 扣200
      * @Param: []
      * @return: void
      * @Author: VICooL
      */
     @Test
-    void testUpdateWrapper(){
-        // 1.要更新的数据
-        User user = new User();
-        user.setBalance (2000);
-        // 2.更新的条件
-        QueryWrapper<User> wrapper = new QueryWrapper<User>()
-                .eq("username","jack");
-        // 3.执行更新
-        userMapper.update(user,wrapper);
+    void testUpdateWrapper() {
+        List<Long> ids = Arrays.asList(1L, 2L, 4L);
+        UpdateWrapper<User> wrapper = new UpdateWrapper<User>()
+                .setSql("balance = balance - 200")
+                .in("id", ids);
+        // 执行更新
+        userMapper.update(null, wrapper);
     }
 
+    /**
+     * @Description: 查询出名字中带o的，存款大于等于1000元的人的id、username、info、balance字段
+     * LambdaQueryWrapper
+     * @Param: []
+     * @return: void
+     * @Author: VICooL
+     */
+    @Test
+    void testLambdaQueryWrapper() {
+        // 1.构造查询条件
+        LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper<User>()
+                .select(User::getId, User::getUsername, User::getInfo, User::getBalance)
+                .like(User::getUsername, "o")
+                .ge(User::getBalance, 1000);
+        // 查询
+        List<User> users = userMapper.selectList(wrapper);
+        users.forEach(System.out::println);
+    }
 
-
-
-
-
-
+    /**
+     * @Description: 自定义sql,更新id为1, 2, 4的用户的余额, 扣200
+     * @Param: []
+     * @return: void
+     * @Author: VICooL
+     * summary: 1.基于Wrapper构建条件
+     *          2.在mapper方法参数中用Param注解接收wrapper变量,必须是ew
+     *          3.自定义SQL,并使用Wrapper条件
+     */
+    @Test
+    void testCustomerSqlUpdate() {
+        // 1.更新条件
+        List<Long> ids = Arrays.asList(1L, 2L, 4L);
+        int amount = 200;
+        // 2.自定义条件
+        LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper<User>()
+                .in(User::getId, ids);
+        // 3.调用自定义SQL方法
+        userMapper.customerSqlUpdate(wrapper, amount);
+    }
 
 
 
